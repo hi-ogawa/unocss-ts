a6d6c25a7fb18271d3eec352e0b69ca07ade6ec6
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-01-29 11:44:59 +0900

    chore: wip transform

---
+++ src/transform.ts
@@ -0,0 +1,6 @@
+// TODO: without babel? regex machinary?
+//       constant folding? (https://github.com/babel/minify/blob/8f9c7c0a32103653bd6793d0ce44730f937e1ddf/packages/babel-plugin-minify-constant-folding/README.md)
+//       force special terminater on API? e.g. tw.(....).tw
+export function transform(code: string): string {
+  return code;
+}
