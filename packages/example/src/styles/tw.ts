25a1943699fb14c056f46ace366e175fedc6f26b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-01-29 21:37:38 +0900

    chore: comment

--- packages/example/src/styles/tw.ts
+++ packages/example/src/styles/tw.ts
@@ -1,5 +1,5 @@
 import { createApi } from "@hiogawa/unocss-typescript-dsl";
 import type { Api } from "./tw-api";
 
-// `createApi` itself is so tiny, so it doesn't matter whether we eliminate it from bundle
+// `createApi` (aka runtime) itself is so tiny, so it doesn't matter whether we eliminate it from bundle
 export const tw = createApi() as Api;
